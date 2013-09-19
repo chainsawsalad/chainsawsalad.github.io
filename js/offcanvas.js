3c6823e4c3a339eeb71b26e7ba36807d453398c9
Jeff Colombo <git@colombo.us>
2013-09-18 22:36:18 -0400

    Make project navigation work better

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -3,3 +3,20 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+//scroll with the fixed header in mind when using the project navigator
+$('#project-nav a').click(function(event) {
+    event.preventDefault();
+
+    var hash = $(this).attr('href'),
+      $body = $('body');
+
+    if(hash != window.location.hash) {
+      window.location.hash = hash;
+    }
+
+    $body.scrollTop(
+      $(hash).offset().top - $body.attr('data-offset') + 1
+    );
+
+});
